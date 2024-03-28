76459621450f8382a5ce06d8b91d2be672711ac8
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-03-28 09:43:14 +0100

    Change to dynamic

--- Src/Mediatoy/Mediatoy/Form1.cs
+++ Src/Mediatoy/Mediatoy/Form1.cs
@@ -5,11 +5,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using WebView2 = Microsoft.Web.WebView2.WinForms.WebView2;
-using System.Diagnostics;
 using System.Drawing;
 using System.ComponentModel;
-using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Mediatoy
 {
@@ -33,6 +32,9 @@ namespace Mediatoy
         public static int vkCode, scanCode;
         public static bool KeyboardHookButtonDown, KeyboardHookButtonUp;
         public static bool starting = true;
+        public static List<string> links = new List<string>(), pictures = new List<string>();
+        public static List<PictureBox> pictureboxes = new List<PictureBox>();
+        public static PictureBox pbmargin = new PictureBox(); 
         public static int[] wd = { 2, 2, 2, 2 };
         public static int[] wu = { 2, 2, 2, 2 };
         public static void valchanged(int n, bool val)
@@ -64,7 +66,29 @@ namespace Mediatoy
             cy = Screen.PrimaryScreen.Bounds.Height;
             this.Size = new Size(cx, cy);
             this.Location = new Point(x, y);
-            SetStyle();
+            this.BackgroundImage = Bitmap.FromFile("background.gif");
+            using (StreamReader file = new StreamReader("mediatoy.txt"))
+            {
+                while (true)
+                {
+                    string link = file.ReadLine();
+                    string picture = file.ReadLine();
+                    if (link == "" | picture == "")
+                    {
+                        file.Close();
+                        break;
+                    }
+                    else
+                    {
+                        links.Add(link);
+                        pictures.Add(picture);
+                        pictureboxes.Add(new PictureBox());
+                    }
+                }
+            }
+            FillBox();
+            SetStyle(); 
+            AddStyle();
             CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-gpu --disable-gpu-compositing", "en");
             CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
             await webView21.EnsureCoreWebView2Async(environment);
@@ -82,7 +106,6 @@ namespace Mediatoy
             webView21.KeyDown += WebView21_KeyDown;
             webView21.DefaultBackgroundColor = Color.Black;
             this.Controls.Add(webView21);
-            this.webView21.Hide();
         }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
@@ -254,181 +277,70 @@ namespace Mediatoy
         {
             webView21.Dispose();
         }
-        private void pbmolotov_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://app.molotov.tv/channels/");
-            RemoveStyle();
-        }
-        private void pbcrunchyroll_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.crunchyroll.com/fr/");
-            RemoveStyle();
-        }
-        private void pbnetflix_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.netflix.com/");
-            RemoveStyle();
-        }
-        private void pbhbo_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.hbo.com/");
-            RemoveStyle();
-        }
-        private void pbparamount_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.paramountplus.com/");
-            RemoveStyle();
-        }
-        private void pbcanal_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.canalplus.com/");
-            RemoveStyle();
-        }
-        private void pbdisney_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.disneyplus.com/");
-            RemoveStyle();
-        }
-        private void pbocs_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.ocs.fr/");
-            RemoveStyle();
-        }
-        private void pbprimevideo_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.primevideo.com/");
-            RemoveStyle();
-        }
-        private void pbtf1_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.tf1.fr/");
-            RemoveStyle();
-        }
-        private void pbpluto_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://pluto.tv/");
-            RemoveStyle();
-        }
-        private void pbweather_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://meteofrance.com/");
-            RemoveStyle();
-        }
-        private void pbyoutube_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.youtube.com/");
-            RemoveStyle();
-        }
-        private void pbspotify_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://open.spotify.com/");
-            RemoveStyle();
-        }
-        private void pbdeezer_Click(object sender, EventArgs e)
-        {
-            webView21.Source = new Uri("https://www.deezer.com/us/");
-            RemoveStyle();
-        }
         private void RemoveStyle()
         {
             this.webView21.Show();
-            this.Controls.Remove(pbmolotov);
-            this.Controls.Remove(pbcrunchyroll);
-            this.Controls.Remove(pbnetflix);
-            this.Controls.Remove(pbhbo);
-            this.Controls.Remove(pbparamount);
-            this.Controls.Remove(pbcanal);
-            this.Controls.Remove(pbdisney);
-            this.Controls.Remove(pbocs);
-            this.Controls.Remove(pbprimevideo);
-            this.Controls.Remove(pbtf1);
-            this.Controls.Remove(pbpluto);
-            this.Controls.Remove(pbweather);
-            this.Controls.Remove(pbyoutube);
-            this.Controls.Remove(pbspotify);
-            this.Controls.Remove(pbdeezer);
+            foreach (PictureBox picturebox in pictureboxes)
+            {
+                this.Controls.Remove(picturebox);
+            }
+            this.Controls.Remove(pbmargin);
         }
         private void AddStyle()
         {
             this.webView21.Hide();
-            this.Controls.Add(pbmolotov);
-            pbmolotov.BringToFront();
-            this.Controls.Add(pbcrunchyroll);
-            pbcrunchyroll.BringToFront();
-            this.Controls.Add(pbnetflix);
-            pbnetflix.BringToFront();
-            this.Controls.Add(pbhbo);
-            pbhbo.BringToFront();
-            this.Controls.Add(pbparamount);
-            pbparamount.BringToFront();
-            this.Controls.Add(pbcanal);
-            pbcanal.BringToFront();
-            this.Controls.Add(pbdisney);
-            pbdisney.BringToFront();
-            this.Controls.Add(pbocs);
-            pbocs.BringToFront();
-            this.Controls.Add(pbprimevideo);
-            pbprimevideo.BringToFront();
-            this.Controls.Add(pbtf1);
-            pbtf1.BringToFront();
-            this.Controls.Add(pbpluto);
-            pbpluto.BringToFront();
-            this.Controls.Add(pbweather);
-            pbweather.BringToFront();
-            this.Controls.Add(pbyoutube);
-            pbyoutube.BringToFront();
-            this.Controls.Add(pbspotify);
-            pbspotify.BringToFront();
-            this.Controls.Add(pbdeezer);
-            pbdeezer.BringToFront();
+            foreach (PictureBox picturebox in pictureboxes)
+            {
+                this.Controls.Add(picturebox);
+                picturebox.BringToFront();
+            }
+            this.Controls.Add(pbmargin);
+            pbmargin.BringToFront();
         }
         private void SetStyle()
         {
-            pbmolotov.Cursor = Cursors.Hand;
-            pbcrunchyroll.Cursor = Cursors.Hand;
-            pbnetflix.Cursor = Cursors.Hand;
-            pbhbo.Cursor = Cursors.Hand;
-            pbparamount.Cursor = Cursors.Hand;
-            pbcanal.Cursor = Cursors.Hand;
-            pbdisney.Cursor = Cursors.Hand;
-            pbocs.Cursor = Cursors.Hand;
-            pbprimevideo.Cursor = Cursors.Hand;
-            pbtf1.Cursor = Cursors.Hand;
-            pbpluto.Cursor = Cursors.Hand;
-            pbweather.Cursor = Cursors.Hand;
-            pbyoutube.Cursor = Cursors.Hand;
-            pbspotify.Cursor = Cursors.Hand;
-            pbdeezer.Cursor = Cursors.Hand;
-            this.pbmolotov.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbcrunchyroll.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbnetflix.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbhbo.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbparamount.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbcanal.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbdisney.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbocs.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbprimevideo.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbtf1.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbpluto.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbweather.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbyoutube.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbspotify.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbdeezer.Size = new System.Drawing.Size(cx / 16, cy / 9);
-            this.pbmolotov.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 1 / 9 + this.pbmolotov.Size.Width * 0, (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbcrunchyroll.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 2 / 9 + this.pbmolotov.Size.Width * 1, (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbnetflix.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 3 / 9 + this.pbmolotov.Size.Width * 2, (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbhbo.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 4 / 9 + this.pbmolotov.Size.Width * 3,   (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbparamount.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 5 / 9 + this.pbmolotov.Size.Width * 4, (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbcanal.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 6 / 9 + this.pbmolotov.Size.Width * 5, (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbdisney.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 7 / 9 + this.pbmolotov.Size.Width * 6, (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbocs.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 8 / 9 + this.pbmolotov.Size.Width * 7, (cy - this.pbmolotov.Size.Height * 5) * 1 / 6 + this.pbmolotov.Size.Height * 0);
-            this.pbprimevideo.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 1 / 9 + this.pbmolotov.Size.Width * 0, (cy - this.pbmolotov.Size.Height * 5) * 2 / 6 + this.pbmolotov.Size.Height * 1);
-            this.pbtf1.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 2 / 9 + this.pbmolotov.Size.Width * 1, (cy - this.pbmolotov.Size.Height * 5) * 2 / 6 + this.pbmolotov.Size.Height * 1);
-            this.pbpluto.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 3 / 9 + this.pbmolotov.Size.Width * 2, (cy - this.pbmolotov.Size.Height * 5) * 2 / 6 + this.pbmolotov.Size.Height * 1);
-            this.pbweather.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 4 / 9 + this.pbmolotov.Size.Width * 3, (cy - this.pbmolotov.Size.Height * 5) * 2 / 6 + this.pbmolotov.Size.Height * 1);
-            this.pbyoutube.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 5 / 9 + this.pbmolotov.Size.Width * 4, (cy - this.pbmolotov.Size.Height * 5) * 2 / 6 + this.pbmolotov.Size.Height * 1);
-            this.pbspotify.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 6 / 9 + this.pbmolotov.Size.Width * 5, (cy - this.pbmolotov.Size.Height * 5) * 2 / 6 + this.pbmolotov.Size.Height * 1);
-            this.pbdeezer.Location = new Point((cx - this.pbmolotov.Size.Width * 8) * 7 / 9 + this.pbmolotov.Size.Width * 6, (cy - this.pbmolotov.Size.Height * 5) * 2 / 6 + this.pbmolotov.Size.Height * 1);
+            bool justnewline = false;
+            int lx = 0;
+            int ly = 0;
+            foreach (PictureBox picturebox in pictureboxes)
+            {
+                picturebox.Cursor = Cursors.Hand;
+                picturebox.Size = new System.Drawing.Size(cx / 16, cy / 9);
+                picturebox.BackColor = System.Drawing.Color.Transparent;
+                picturebox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                picturebox.Location = new Point((cx - picturebox.Size.Width * 8) * (lx + 1) / 9 + picturebox.Size.Width * lx, (cy - picturebox.Size.Height * 5) * (ly + 1) / 6 + picturebox.Size.Height * ly);
+                lx++;
+                if (lx > 7) 
+                {
+                    lx = 0;
+                    ly++;
+                    justnewline = true;
+                }
+                else
+                    justnewline = false;
+            }
+            lx = 0;
+            if (!justnewline)
+                ly++;
+            pbmargin.Size = new System.Drawing.Size(cx / 16, cy / 9);
+            pbmargin.BackColor = System.Drawing.Color.Transparent;
+            pbmargin.Location = new Point((cx - pbmargin.Size.Width * 8) * (lx + 1) / 9 + pbmargin.Size.Width * lx, (cy - pbmargin.Size.Height * 5) * (ly + 1) / 6 + pbmargin.Size.Height * ly);
         }
+        private void FillBox()
+        {
+            foreach (var (picturebox, index) in pictureboxes.WithIndex())
+            {
+                picturebox.Click += (sender, e) =>
+                {
+                    webView21.Source = new Uri(links[index]);
+                    RemoveStyle();
+                };
+                picturebox.BackgroundImage = Bitmap.FromFile(pictures[index]);
+            }
+        }
+    }
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self) => self.Select((item, index) => (item, index));
     }
 }
