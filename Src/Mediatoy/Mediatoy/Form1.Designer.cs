9474403c23c14ff3b9a0ac546662024e659db456
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-05 23:57:48 +0200

    Add a feature

--- Src/Mediatoy/Mediatoy/Form1.Designer.cs
+++ Src/Mediatoy/Mediatoy/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // timer1
@@ -38,6 +39,12 @@
             this.timer1.Enabled = true;
             this.timer1.Interval = 6000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 60;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
@@ -63,6 +70,7 @@
 
         #endregion
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
